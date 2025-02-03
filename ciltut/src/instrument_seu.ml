555f6f9c4346f62ebbcd209a5697748c2a644371
ganeshsprabhu <31914466+ganeshsprabhu@users.noreply.github.com>
2025-02-04 01:09:46 +0530

    generalized for variables

--- ciltut/src/instrument_seu.ml
+++ ciltut/src/instrument_seu.ml
@@ -11,12 +11,13 @@ let rec has_variable (vname : string) (e : exp) : bool =
   | CastE (_, e1) -> has_variable vname e1
   | _ -> false
 
-(* Function to create simulate_seu_main(&x) call *)
-let create_seu_call (vi : varinfo) (loc : location) : instr =
-  let seu_fun = makeGlobalVar "simulate_seu_main" (TFun(voidType, Some [("x", TPtr(intType, []), [])], false, [])) in
+(* Function to create simulate_seu_main(&var) call dynamically *)
+let create_seu_call (target_var : string) (vi : varinfo) (loc : location) : instr =
+  let seu_fun = makeGlobalVar "simulate_seu_main" 
+    (TFun(voidType, Some [(target_var, TPtr(intType, []), [])], false, [])) in
   Call (None, Lval (Var seu_fun, NoOffset), [AddrOf (Var vi, NoOffset)], loc)
 
-(* Visitor class to insert simulate_seu_main before each use of x *)
+(* Visitor class to insert simulate_seu_main before each use of target_var *)
 class seuInstrumentationVisitor (target_var : string) = object
   inherit nopCilVisitor
 
@@ -27,21 +28,22 @@ class seuInstrumentationVisitor (target_var : string) = object
         List.iter (fun i ->
           match i with
           | Set ((Var vi, NoOffset), rhs, loc) when has_variable target_var rhs ->
-              new_instrs := create_seu_call vi loc :: i :: !new_instrs
+              new_instrs := create_seu_call target_var vi loc :: i :: !new_instrs
           | Call (_, Lval (Var vi, NoOffset), args, loc) 
             when List.exists (has_variable target_var) args ->
-              new_instrs := create_seu_call vi loc :: i :: !new_instrs
+              new_instrs := create_seu_call target_var vi loc :: i :: !new_instrs
           | _ -> new_instrs := i :: !new_instrs
         ) il;
         ChangeTo (mkStmt (Instr (List.rev !new_instrs)))
     | If (cond, b1, b2, loc) when has_variable target_var cond ->
-        let new_stmt = mkStmt (Instr [create_seu_call (makeLocalVar (emptyFunction "temp") target_var intType) loc]) in
+        let temp_var = makeLocalVar (emptyFunction "temp") target_var intType in
+        let new_stmt = mkStmt (Instr [create_seu_call target_var temp_var loc]) in
         let new_block = mkBlock (new_stmt :: [s]) in
         ChangeTo (mkStmt (Block new_block))
     | _ -> DoChildren
 end
 
-(* Process each function, ensuring `simulate_seu_main(&x);` is added *)
+(* Process each function, ensuring simulate_seu_main(&var); is added dynamically *)
 let process_function (fd : fundec) (target_var : string) : unit =
   let has_target = List.exists (fun vi -> vi.vname = target_var) (fd.sformals @ fd.slocals) in
   if has_target then ignore (visitCilFunction (new seuInstrumentationVisitor target_var) fd)
@@ -50,7 +52,7 @@ let process_function (fd : fundec) (target_var : string) : unit =
 let () =
   let input_file = "../test/input.c" in
   let output_file = "../test/output.c" in
-  let target_var = "x" in  (* Variable to monitor *)
+  let target_var = Sys.argv.(1) in  (* Take variable name from command-line argument *)
 
   let f = Frontc.parse input_file () in
   iterGlobals f (function
